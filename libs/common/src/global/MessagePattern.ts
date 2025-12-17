f6b7047f54f405d87fef9f747b6f04d3e60b9ed6
Chidi <chidiebere@zap.africa>
2025-12-17 08:39:16 +0100

    feat(vendor-service): add GET_VENDOR_BY_ID message pattern and implement vendor retrieval in controller

--- libs/common/src/global/MessagePattern.ts
+++ libs/common/src/global/MessagePattern.ts
@@ -11,6 +11,7 @@ export const MessagePatterns = {
     VENDOR_SERVICE: {
         CREATE_VENDOR: { cmd: 'create_vendor' },
         GET_VENDOR_BY_USER_ID: { cmd: 'get_vendor_by_user_id' },
+        GET_VENDOR_BY_ID: { cmd: 'get_vendor_by_id' },
         VALIDATE_VENDOR: { cmd: 'validate_vendor' },
     },
 }
