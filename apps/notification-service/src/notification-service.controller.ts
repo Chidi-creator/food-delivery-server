573c24cfbae53f3eab85c5ff0e4b3ad612fe8294
Chidi <chidiebere@zap.africa>
2025-12-17 08:18:18 +0100

    feat(notification-service): update notification patterns to use MessagePatterns and enhance notification handling

--- apps/notification-service/src/notification-service.controller.ts
+++ apps/notification-service/src/notification-service.controller.ts
@@ -3,14 +3,13 @@ import { EmailNotificationService } from './email-notification-service.service';
 import { EventPattern } from '@nestjs/microservices';
 import { NotificationMessage } from '@chidi-food-delivery/common';
 import { NotificationManager } from './notifiation-manager';
+import { MessagePatterns } from '@chidi-food-delivery/common/global/MessagePattern';
 
 @Controller()
 export class NotificationServiceController {
-  constructor(
-    private readonly notificationManager: NotificationManager,
-  ) {}
+  constructor(private readonly notificationManager: NotificationManager) {}
 
-  @EventPattern('user_registered')
+  @EventPattern(MessagePatterns.NOTIFICATION_SERVICE.SEND_NOTIFICATION)
   async handleUserRegisteredEvent(data: NotificationMessage) {
     await this.notificationManager.sendNotification(data);
   }
